277860b63f90f3a7cd82c17d5021fe6986bff4c1
CorniiDog <connor.sw.personal@gmail.com>
2025-09-05 12:06:36 -0500

    wip core

--- lightning_parser_overlays/__init__.py
+++ lightning_parser_overlays/__init__.py
@@ -1,24 +1,11 @@
-from .config_and_parser import (
-    LightningConfig,
-    cache_and_parse,
-    get_events,
-    bucket_dataframe_lightnings,
-    display_stats,
-    export_as_csv,
-    export_general_stats,
-    export_all_strikes,
-    export_strike_stitchings,
+from .core import (
+    FigureDetails, 
+    ColorbarDetails, 
+    Overlay
 )
 
 __all__ = [
-    "LightningConfig",
-    "cache_and_parse",
-    "get_events",
-    "bucket_dataframe_lightnings",
-    "display_stats",
-    "export_as_csv",
-    "export_general_stats",
-    "export_all_strikes",
-    "export_strike_stitchings",
-    "number_crunchers",
+  "FigureDetails", 
+  "ColorbarDetails", 
+  "Overlay"
 ]
---
+++ lightning_parser_overlays/goes_overlays.py
@@ -0,0 +1,4 @@
+
+# Access overlay objects
+from .core import FigureDetails, ColorbarDetails, Overlay
+
